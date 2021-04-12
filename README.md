219f7bfc674d22e16f434529425093356226243f
Brad Oyler <brad.oyler@gopuff.com>
2021-04-12 09:08:14 -0400

    add DLQ drainer, update servicebus version

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Working examples of javascript sdk usage components I actively use
 
 ### Components:
 
-- [@azure/service-bus@1.1.5](https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/servicebus/service-bus) -> [MSFT docs](https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-nodejs-how-to-use-topics-subscriptions)
+- [@azure/service-bus@1.1.10](https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/servicebus/service-bus) -> [MSFT docs](https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-nodejs-how-to-use-topics-subscriptions)
 
 #### Coming soon...
 - [@azure/event-hubs](https://github.com/Azure/azure-sdk-for-js/tree/master/sdk/eventhub/event-hubs)
---
+++ src/servicebus/receiveDeadLetters.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const { ServiceBusClient, ReceiveMode, QueueClient, TopicClient } = require('@azure/service-bus')
+
+const entityType = process.env.ENTITY_TYPE || 'topic' // or 'queue'
+const connectionString = process.env.SERVICEBUS_CONNECTION_STRING || '<connection string>'
+const entityName = process.env.ENTITY_NAME || '<topic/queue name>'
+const subscriptionName = process.env.SUBSCRIPTION_NAME
+const dlReceiveSize = parseInt(process.env.DEADLETTER_RECEIVE_SIZE) || 2000
+
+const sbClient = ServiceBusClient.createFromConnectionString(connectionString)
+
+async function main () {
+  try {
+    await receiveDeadletters()
+  } finally {
+    await sbClient.close()
+  }
+}
+
+async function receiveDeadletters () {
+  let receiver
+  let entityClient // either QueueClient or SubscriptionClient
+  if (entityType === 'queue') {
+    const deadLetterQueueName = QueueClient.getDeadLetterQueuePath(entityName)
+    entityClient = sbClient.createQueueClient(deadLetterQueueName)
+    receiver = entityClient.createReceiver(ReceiveMode.peekLock)
+  } else if (entityType === 'topic' && subscriptionName) {
+    const deadLetterQueueName = TopicClient.getDeadLetterTopicPath(entityName, subscriptionName)
+    entityClient = sbClient.createSubscriptionClient(deadLetterQueueName)
+    receiver = entityClient.createReceiver(ReceiveMode.receiveAndDelete)
+  } else {
+    return console.log(`You must set ${entityType} options`)
+  }
+
+  const messages = await receiver.receiveMessages(dlReceiveSize)
+
+  if (messages.length > 0) {
+    console.log(`## Rcvd ${messages.length} messages from ${entityName} ${entityType} DLQ:`, new Date(messages[0]._amqpMessage.creation_time))
+
+    // Mark message as complete/processed. Only needed for peekLock ReceiveMode
+    // await receiver.completeMessage(messages[0])
+  } else {
+    console.log('### Error: No messages were received from the DLQ.')
+  }
+
+  await entityClient.close()
+}
+
+main().catch((err) => {
+  console.log('### Error:', err)
+  process.exit(1)
+})
